4d7e557f9710a3a2bd108bc80c490b876c393a1d
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-22 13:41:49 -0300

    feat: implement modal controller service for task creation and editing, update task form modal to handle mode changes

--- src/app/components/task-form-modal/task-form-modal.component.ts
+++ src/app/components/task-form-modal/task-form-modal.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ITaskFormModalData } from '../../interfaces/task-form-modal-data.interface';
+import { DIALOG_DATA } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-task-form-modal',
   imports: [],
   templateUrl: './task-form-modal.component.html',
-  styleUrl: './task-form-modal.component.css'
+  styleUrl: './task-form-modal.component.css',
 })
 export class TaskFormModalComponent {
-
+  readonly _data: ITaskFormModalData = inject(DIALOG_DATA);
 }
