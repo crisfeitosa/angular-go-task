add19cb9c804cafd60a7e4d2c2d511f65ce85c75
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-24 17:11:50 -0300

    feat: add task and comment interfaces, task status enum, and update task service to use typed observables

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { ITask } from '../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   // Tarefas em A fazer
-  private todoTasks$ = new BehaviorSubject<any[]>([]);
+  private todoTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly todoTasks = this.todoTasks$.asObservable();
 
   // Tarefas em Fazendo
-  private doingTasks$ = new BehaviorSubject<any[]>([]);
+  private doingTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doingTasks = this.doingTasks$.asObservable();
 
   // Tarefas em Concluído
-  private doneTasks$ = new BehaviorSubject<any[]>([]);
+  private doneTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doneTasks = this.doneTasks$.asObservable();
 }
