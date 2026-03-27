e2ccae7e8db79f360af1527b4f8b383d0a326138
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-27 12:34:19 -0300

    feat: enhance task management by implementing status updates on card drop

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -10,6 +10,8 @@ import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { TaskService } from '../../services/task.service';
 import { ITask } from '../../interfaces/task.interface';
+import { TaskStatus } from '../../types/task-status';
+import { TaskStatusEnum } from '../../enums/task-status.enum';
 
 @Component({
   selector: 'app-task-list-section',
@@ -20,7 +22,38 @@ import { ITask } from '../../interfaces/task.interface';
 export class TaskListSectionComponent {
   readonly _taskService = inject(TaskService);
 
-  drop(event: CdkDragDrop<ITask[]>) {
+  onCardDrop(event: CdkDragDrop<ITask[]>) {
+    this.moveCardToColumn(event);
+
+    const taskId = event.item.data.id;
+    const taskCurrentStatus = event.item.data.status;
+    const droppedColumn = event.container.id;
+
+    this.updateTaskStatus(taskId, taskCurrentStatus, droppedColumn);
+  }
+
+  private updateTaskStatus(
+    taskId: string,
+    taskCurrentStatus: TaskStatus,
+    droppedColumn: string,
+  ) {
+    let taskNextStatus: TaskStatus;
+    switch (droppedColumn) {
+      case 'to-do-column':
+        taskNextStatus = TaskStatusEnum.TODO;
+        break;
+      case 'doing-column':
+        taskNextStatus = TaskStatusEnum.DOING;
+        break;
+      case 'done-column':
+        taskNextStatus = TaskStatusEnum.DONE;
+        break;
+      default:
+        throw new Error('Coluna não identificada');
+    }
+  }
+
+  private moveCardToColumn(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
