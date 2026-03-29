be8fa415db65db0f5e16064b6721db5bf0284751
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-29 10:31:53 -0300

    feat: implement task deletion functionality in task card component

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -113,6 +113,15 @@ export class TaskService {
     }
   }
 
+  deleteTask(taskId: string, taskCurrentStatus: TaskStatus) {
+    const currentTaskList = this.getTaskListByStatus(taskCurrentStatus);
+
+    const newTaskList = currentTaskList.value.filter(
+      (task) => task.id !== taskId,
+    );
+    currentTaskList.next(newTaskList);
+  }
+
   private getTaskListByStatus(taskStatus: TaskStatus) {
     const taskListObj = {
       [TaskStatusEnum.TODO]: this.todoTasks$,
