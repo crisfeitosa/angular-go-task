ff1c4452d4584dc2df8b365635486751fbdce98c
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-29 11:08:54 -0300

    feat: remove unused imports from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
-import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
-import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
 
 @Component({
   selector: 'app-root',
